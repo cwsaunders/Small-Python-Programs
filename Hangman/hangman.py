ede69f4f9d8ff681e0694729e6563f1a706b1710
Christian Saunders <noname45688@gmail.com>
2020-02-03 20:07:04 -0500

    Create hangman.py

---
+++ Hangman/hangman.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+import random
+import string
+
+# Returns random word from text file
+def getword():
+    return random.choice(open("words.txt").readline().split())
+
+
+# Introduction
+print('Welcome to the hangman game of your dreams!\n')
+
+
+word = getword()
+print(f'Your word is {len(word)} characters long')
+print('You have 5 body parts to spare')
+
+while True:
+    try:
+        userguess = str(input('Enter your guess!'))
+        if userguess == len(1) and userguess == string.ascii_letters():
+            break
+        else:
+            print('Error! Your input must be a letter, and only 1 digit in length')
+    except TypeError:
+        print('Error! Your input must be a letter, and only 1 digit in length')
