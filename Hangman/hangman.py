37c1aa9777770a4ef1b2a8a068a005c530bd422f
Christian Saunders <noname45688@gmail.com>
2020-02-05 02:28:54 -0500

    Update hangman.py

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -32,15 +32,34 @@ print('Welcome to the hangman game of your dreams!\n')
 
 
 word = getword()
+
+# This below list is to iterate through with guesses
+wordlist = list(word)
+
+for x in range(wordlist):
+    hiddenlist[x] = '*'
+
+
 print(f'Your word is {len(word)} characters long')
 print('You have 5 body parts to spare')
+print(*hiddenlist, end=" ")
+print('is your word!')
 
-while True:
-    try:
-        userguess = str(input('Enter your guess!'))
-        if len(userguess) is 1 and userguess.isalpha():
-            break
-        else:
+finished = False
+
+while not finished:
+    while True:
+        try:
+            userguess = str(input('Enter your guess!'))
+            if len(userguess) is 1 and userguess.isalpha():
+                break
+            else:
+                print('Error! Your input must be a letter, and only 1 digit in length')
+        except TypeError:
             print('Error! Your input must be a letter, and only 1 digit in length')
-    except TypeError:
-        print('Error! Your input must be a letter, and only 1 digit in length')
+    for x in range(wordlist):
+        if userguess is wordlist[x]:
+            hiddenlist[x] = userguess
+            worldlist.remove(f'{userguess}')
+            print(f'Your guess ({userguess}), was correct!')
+            
