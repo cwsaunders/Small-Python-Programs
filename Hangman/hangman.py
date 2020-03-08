37b553284c05b1959d8c239d8b56b8f3c8b176ea
Christian Saunders <noname45688@gmail.com>
2020-03-08 14:34:36 -0400

    Update hangman.py

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python3
 
-# NOTE: HANGMAN GAME NOT COMPLETE
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
 import random
 import string
 import os
@@ -36,7 +21,6 @@ word = getword()
 
 # This below list is to iterate through with guesses
 wordlist = list(word)
-wordlist2 = list(word)
 
 # List to show player unguessed and guessed letters
 hiddenlist = []
@@ -49,13 +33,13 @@ print('You have 5 body parts to spare')
 print(*hiddenlist, end=" ")
 print('is your word!')
 
-# Guess counter
+# Failed guess counter
 counter = 0
-
+counter2 = 0
 # Checks to see if entire word is guessed or game over
 finished = False
 
-while not finished:
+while (not finished and counter < 5):
     # Error checking for user guess
     while True:
         try:
@@ -66,35 +50,31 @@ while not finished:
                 print('Error! Your input must be a letter, and only 1 digit in length')
         except TypeError:
             print('Error! Your input must be a letter, and only 1 digit in length')
-    # Edits user-shown list when letter guessed
-    # Shortenes list of letters to guess when correct letter guessed
-    # Increases counter if a 'life' is lost
-    for x in range(len(wordlist)):
-        if userguess is wordlist[x]:
-            # Issue: Same letter does not fill in twice 
-            hiddenlist[wordlist2.index(userguess)] = userguess
-            wordlist.remove(f'{userguess}')
-            moreletters=True
-            while moreletters:
-                for x in range(len(wordlist)):
-                    # Trying to check if a letter occurs more
-                    # than once and if it does then fill it in
-                    # every spot
-                    # ******* NOT DONE YET
-            print(f'Your guess ({userguess}), was correct!')
-            break
-        elif userguess is not any(wordlist):
+    for i in range(len(wordlist)):
+        if(userguess == wordlist[i]):
+            hiddenlist[i] = wordlist[i]
+            counter2 += 1
+        elif(i == (len(wordlist)-1) and userguess != wordlist[i] and counter2 == 0):
             counter += 1
-            print(f'Your letter was not in the word! You have {5-counter} attempts left')
+            print(f'Incorrect guess! You have {5-counter} body parts left!')
+            print(*hiddenlist, end=" ")
             break
-    # Checks if attempts out or word guessed
-    if len(wordlist) is 0 or counter > 5:
+        
+        if(i==(len(wordlist)-1) and counter2 > 0):
+            while(counter2>0):
+                counter2 -=1
+            print(*hiddenlist, end=" ")
+    for i in (range(len(hiddenlist))):
+        anyasterisk = 0
+        if(hiddenlist[i] == '*'):
+            anyasterisk += 1
+            break
+    if(anyasterisk == 0):
         finished = True
-    print('Word: ', end='')
-    print(*hiddenlist)
+    if(anyasterisk >= 1):
+        anyasterisk = 0
+if(finished == False):
+    print('You lost!')
+elif(finished == True):
+    print('You won!!!!')
 
-    # Checks if win vs loss
-    if counter > 5:
-        print('Game over! You lose')
-    elif len(wordlist) is 0:
-        print('You win! Congratulations!')
